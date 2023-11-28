2831b80c1805f520f61531d4b7f3ac2581dc40c9
Anna <anya.konnowa2015@gmail.com>
2023-11-28 20:01:45 +0700

    add test for sign up, reset password and changing roles logics

---
+++ app/tests/Unit/Model/User/Entity/User/Reset/RequestTest.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\Model\User\Entity\User\Reset;
+
+use App\Model\User\Entity\User\Email;
+use App\Model\User\Entity\User\Id;
+use App\Model\User\Entity\User\ResetToken;
+use App\Model\User\Entity\User\User;
+use App\Tests\Builder\User\UserBuilder;
+use PHPUnit\Framework\TestCase;
+
+class RequestTest extends TestCase
+{
+    public function testSuccess(): void
+    {
+        $now = new \DateTimeImmutable();
+        $token = new ResetToken('token', $now->modify('+1 day'));
+
+        $user = (new UserBuilder())->confirmed()->build();
+
+        $user->requestPasswordReset($token, $now);
+
+        self::assertNotNull($user->getResetToken());
+    }
+
+    public function testAlready(): void
+    {
+        $now = new \DateTimeImmutable();
+        $token = new ResetToken('token', $now->modify('+1 day'));
+
+        $user = (new UserBuilder())->confirmed()->build();
+
+        $user->requestPasswordReset($token, $now);
+
+        $this->expectExceptionMessage('Сброс пароля уже запрошен.');
+        $user->requestPasswordReset($token, $now);
+    }
+
+    public function testExpired(): void
+    {
+        $now = new \DateTimeImmutable();
+
+        $user = (new UserBuilder())->confirmed()->build();
+
+        $token1 = new ResetToken('token', $now->modify('+1 day'));
+        $user->requestPasswordReset($token1, $now);
+
+        self::assertEquals($token1, $user->getResetToken());
+
+        $token2 = new ResetToken('token', $now->modify('+3 day'));
+        $user->requestPasswordReset($token2, $now->modify('+2 day'));
+
+        self::assertEquals($token2, $user->getResetToken());
+    }
+
+    public function testNotConfirmed(): void
+    {
+        $now = new \DateTimeImmutable();
+        $token = new ResetToken('token', $now->modify('+1 day'));
+
+        $user = (new UserBuilder())->build();
+
+        $this->expectExceptionMessage('Пользователь не подтверждён.');
+        $user->requestPasswordReset($token, $now);
+    }
+}
